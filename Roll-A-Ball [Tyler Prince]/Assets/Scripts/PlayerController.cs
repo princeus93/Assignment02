8033c790e8683e888a332a40d3f08f327a85b933
princeus93 <69991982+princeus93@users.noreply.github.com>
2021-02-03 11:41:02 -0600

    Finished Roller-Ball Tutorial

--- Roll-A-Ball [Tyler Prince]/Assets/Scripts/PlayerController.cs
+++ Roll-A-Ball [Tyler Prince]/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,13 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;  //Required to use "Text" object and other UI elements
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 800.0f; //public so we can change this value in the inspector
-
+    public Text scoreText;  //"scoreText" will store our UI Text object
+    private int count = 0;  //"count" will keep track of how many cubes we picked up
+    public Text winText;
     // Update is called once per frame
     void FixedUpdate()  //Infinite loop while we are in play mode
     {
@@ -21,6 +24,14 @@ public class PlayerController : MonoBehaviour
         if(other.gameObject.tag == "PickUp")
         {
             other.gameObject.SetActive(false);
+            count += 1;  //Adds 1 to count each time we pick up a cube
+
+            scoreText.text = "Score: " + count; //Updates the text property of scoreText
+            if(count >= 16)
+            {
+                winText.gameObject.SetActive(true);
+            }
         }
+        
     }
 }
